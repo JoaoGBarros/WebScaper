f330820435f6ba6b6ead71eae712cf0551d513c6
Joao Gabriel de Barros <38754105+JoaoGBarros@users.noreply.github.com>
2021-07-20 01:08:59 -0300

    Add files via upload

--- web-scraper-ML.py
+++ web-scraper-ML.py
@@ -24,6 +24,8 @@ def produtoCerto(produto_encontrado, produto_procurado):
 
 
 if __name__ == '__main__':
+    options = webdriver.ChromeOptions()
+    options.add_experimental_option('excludeSwitches', ['enable-logging'])
     site = "https://www.mercadolivre.com.br/" #Site que sera utilizado
     print("------- Servico de Busca Automatizada no site " + site + " -------")
     produto = input("Produto que deseja buscar: ")
@@ -35,31 +37,32 @@ if __name__ == '__main__':
     lista_fretes = []
     local = os.getcwd()  # Pede ao usuario o local do Chrome Driver
     local = local + "/chromedriver.exe"
-    print("Espere a janela do Chrome fechar para abrir o arquivo .csv, caso o programa seja executado no terminal, o erro que aparece pode ser desconsiderado")
-    navegador = webdriver.Chrome(local)   # Abre o executavel do chrome driver
+    print("Espere a janela do Chrome fechar para abrir o arquivo .csv")
+    navegador = webdriver.Chrome(local, options=options)   # Abre o executavel do chrome driver
     navegador.minimize_window()
     navegador.get(site)  #Entra no site
     pesquisa = navegador.find_element_by_class_name('nav-search-input')  # Procura pelo elemento da barra de pesquisa
     # utilizando sua classe no html
     pesquisa.send_keys(produto)  # Digita na barra de pesquisa o produto
     pesquisa.send_keys(Keys.RETURN)  # ENTER
-    navegador.implicitly_wait(2)  # Espera 2 segundos antes de comecar a coleta de dados para dar tempo da pagina
+    navegador.implicitly_wait(5)  # Espera 2 segundos antes de comecar a coleta de dados para dar tempo da pagina
     # carregar completamente
     conteudo = navegador.page_source
     soup = BeautifulSoup(conteudo, features="html.parser")  # Cria um novo objeto de BeautifulSoup contendo as
     # informacoes da pagina
     existe_proxima = True  # Inicia a condicao para o loop
 
+
     while existe_proxima:
         proxima = None  # Inicializa a verificacao para caso exista uma proxima pagina
 
         #  Faz a busca dentro da div que possua a classe especificada
-        for a in soup.find_all('div', attrs={'class': 'andes-card andes-card--flat andes-card--default ui-search-result ui-search-result--core andes-card--padding-default andes-card--animated'}):
+        for a in soup.find_all('div', attrs={'class': 'ui-search-result__wrapper'}):
             # a.find -> De todos os elementos presentes dentro da div, procura os com as especificacoes
             nome = a.find('div', attrs={'class': 'ui-search-item__group ui-search-item__group--title'})
             if produtoCerto(nome.text, produto):
                 #  Caso a condicao seja verdadeira, armazena os dados do anuncio
-                link = a.find('a', attrs={'class': 'ui-search-result__content ui-search-link'})
+                link = a.find('a', attrs={'class': 'ui-search-link'})
                 if "click" not in link.attrs['href']:
                     lista_links.append(link.attrs['href'])
                 preco = a.find('span', attrs={'class': 'price-tag-amount'})
@@ -79,6 +82,7 @@ if __name__ == '__main__':
         # Caso a proxima pagina nao exista, o loop eh finalizado
         if isinstance(proxima, type(None)):
             existe_proxima = False
+            navegador.quit()  #Fecha o navegador
         else:
             # Caso contrario o programa carrega a proxima pagina
             site = proxima.attrs['href']
@@ -87,9 +91,12 @@ if __name__ == '__main__':
             soup = BeautifulSoup(conteudo, features="html.parser")
 
     # Criacao do .csv utilizando o nome dado pelo usuario
+
+    if not os.path.exists("Relatorios"):
+        os.mkdir("Relatorios")
+    arquivo = os.getcwd() + "/Relatorios/" + arquivo
     data = pd.DataFrame(zip(lista_precos, lista_titulos, lista_fretes, lista_links), columns=['Preco', 'Produto', 'Frete', 'Link'])
     data.to_csv(arquivo, index=False, encoding='utf-8')
 
-    # Fecha o navegador
-    navegador.quit()
+    print("Fim do programa! O .csv esta disponivel na pasta Relatorios!")
 
